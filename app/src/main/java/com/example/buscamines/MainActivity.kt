a80dfdb583a2a2f86e2b518e41c84682dfdceeb0
kikee-sanchez17 <enricmontoya@gmail.com>
2024-02-22 14:53:06 +0100

    segona pujada

--- app/src/main/java/com/example/buscamines/MainActivity.kt
+++ app/src/main/java/com/example/buscamines/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.buscamines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.firestore
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
+        // Access a Cloud Firestore instance
     }
-}
+    }
