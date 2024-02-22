12666a4a3baa9d2a70ff0f1280f12a2705c5dc87
Enric <enricmontoya@gmail.com>
2024-02-22 21:14:55 +0100

    tercera pujada

--- app/src/main/java/com/example/buscamines/MainActivity.kt
+++ app/src/main/java/com/example/buscamines/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.buscamines
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.database.FirebaseDatabase
@@ -12,7 +15,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        // Access a Cloud Firestore instance
+        setContentView(R.layout.activity_main)
+
+        var BTMLOGIN = findViewById<Button>(R.id.BTMLOGIN);
+        var BTMREGISTRO = findViewById<Button>(R.id.BTMREGISTRO);
+        BTMLOGIN.setOnClickListener(){
+            Toast.makeText(this, "click botó login", Toast.LENGTH_LONG).show();
+            val intent = Intent(this,Login::class.java)
+
+            // Iniciar la nueva actividad
+            startActivity(intent)
+        }
+        BTMREGISTRO.setOnClickListener(){
+            Toast.makeText(this, "click botó Registre", Toast.LENGTH_LONG).show();
+            // Crear un Intent para iniciar la actividad deseada
+            val intent = Intent(this,Registro::class.java)
+
+            // Iniciar la nueva actividad
+            startActivity(intent)
+        }
     }
+
+
+
     }
