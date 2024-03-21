72264f56259851c090815fcdb72f7cce30f558e7
kikee-sanchez17 <enricmontoya@gmail.com>
2024-03-21 19:30:42 +0100

    joc empezado

---
+++ app/src/main/java/com/example/buscamines/SeleccioNivell.kt
@@ -0,0 +1,11 @@
+package com.example.buscamines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeleccioNivell : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccio_nivell)
+    }
+}
